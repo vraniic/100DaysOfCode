e26bda23eab36a88876f434d490fcfaa0565f996
vraniic <viktor.vranic02@gmail.com>
2021-12-16 12:28:57 +0100

    15.12.

--- lession28-Ajax&Async/public/scripts/comments.js
+++ lession28-Ajax&Async/public/scripts/comments.js
@@ -1,7 +1,9 @@
 const loadCommentsBtnElement = document.getElementById('load-comments-btn');
 
-function fetchCommentsForPost() {
-    fetch('/')
+function fetchCommentsForPost(event) {
+    const postId = loadCommentsBtnElement.dataset.postId
+    const response = await fetch(`/posts/${postId}/comments`)
+    const reponseData =  await response.json()
 }
 
 loadCommentsBtnElement,addEventListener('click', fetchCommentsForPost)
